a25ad96b94ccb39629341a3f79bb9ca67b413ba9
Chanho Lee <chanho1228@gmail.com>
2025-03-26 09:09:43 +0900

    feat: 결과값 UI라벨 추가

--- nbcampCalculatorWithUI/ViewController.swift
+++ nbcampCalculatorWithUI/ViewController.swift
@@ -6,14 +6,38 @@
 //
 
 import UIKit
+import SnapKit
 
 class ViewController: UIViewController {
-
+    
+    let resultLabel = UILabel()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        print("ViewController가 로딩되었습니다.")
+        
+        configureUI()
     }
-
-
+    
+    private func configureUI() {
+        makeLabel()
+    }
+    
+    private func makeLabel() {
+        resultLabel.text = "12345"
+        resultLabel.backgroundColor = .black
+        resultLabel.textColor = .white
+        resultLabel.textAlignment = .right
+        resultLabel.font = .boldSystemFont(ofSize: 60)
+        
+        view.addSubview(resultLabel)
+        
+        resultLabel.snp.makeConstraints {
+            $0.height.equalTo(100)
+            $0.leading.equalTo(view.snp.leading).inset(30)
+            $0.trailing.equalTo(view.snp.trailing).inset(30)
+            $0.top.equalTo(view.snp.top).inset(200)
+        }
+    }
 }
-
