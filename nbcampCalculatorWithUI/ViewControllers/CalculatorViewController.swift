5b0210ea9651b14a5a65c72cf3f2aacb16964bf9
Chanho Lee <chanho1228@gmail.com>
2025-03-28 15:21:31 +0900

    feat: 뷰와 뷰컨트롤러 사이에 델리게이트 패턴 추가

--- nbcampCalculatorWithUI/ViewControllers/CalculatorViewController.swift
+++ nbcampCalculatorWithUI/ViewControllers/CalculatorViewController.swift
@@ -8,20 +8,19 @@
 import UIKit
 import SnapKit
 
-class CalculatorViewController: UIViewController {
+class CalculatorViewController: UIViewController, CalculatorViewDelegate {
     
     var result = "0"
     var calculatorView = CalculatorView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        calculatorView.delegate = self
         print("ViewController가 로딩되었습니다.")
     }
     
     override func loadView() {
         super.loadView()
-        
 //        var calculatorView = CalculatorView(frame: self.view.frame)
         self.view = calculatorView
     }
@@ -68,81 +67,6 @@ class CalculatorViewController: UIViewController {
     }
     
     @objc
-    func num0ButtonClicked() {
-        resultLabelUpdateByButton(button: "0")
-    }
-    
-    @objc
-    func num1ButtonClicked() {
-        resultLabelUpdateByButton(button: "1")
-    }
-    
-    @objc
-    func num2ButtonClicked() {
-        resultLabelUpdateByButton(button: "2")
-    }
-    
-    @objc
-    func num3ButtonClicked() {
-        resultLabelUpdateByButton(button: "3")
-    }
-    
-    @objc
-    func num4ButtonClicked() {
-        resultLabelUpdateByButton(button: "4")
-    }
-    
-    @objc
-    func num5ButtonClicked() {
-        resultLabelUpdateByButton(button: "5")
-    }
-    
-    @objc
-    func num6ButtonClicked() {
-        resultLabelUpdateByButton(button: "6")
-    }
-    
-    @objc
-    func num7ButtonClicked() {
-        resultLabelUpdateByButton(button: "7")
-    }
-    
-    @objc
-    func num8ButtonClicked() {
-        resultLabelUpdateByButton(button: "8")
-    }
-    
-    @objc
-    func num9ButtonClicked() {
-        resultLabelUpdateByButton(button: "9")
-    }
-    
-    @objc
-    func acButtonClicked() {
-        resultLabelUpdateByButton(button: "ac")
-    }
-    
-    @objc
-    func addButtonClicked() {
-        resultLabelUpdateByButton(button: "+")
-    }
-    
-    @objc
-    func subButtonClicked() {
-        resultLabelUpdateByButton(button: "-")
-    }
-    
-    @objc
-    func mulButtonClicked() {
-        resultLabelUpdateByButton(button: "*")
-    }
-    
-    @objc
-    func divButtonClicked() {
-        resultLabelUpdateByButton(button: "/")
-    }
-    
-    @objc
     func resultButtonClicked() {
         if let calculation = calculate(expression: result) {
             result = String(calculation)
@@ -156,5 +80,9 @@ class CalculatorViewController: UIViewController {
         } else {
             return nil
         }
+    }
+    
+    func buttonClick() {
+        print("출력")
     }
 }
--- nbcampCalculatorWithUI/Views/CalculatorView.swift
+++ nbcampCalculatorWithUI/Views/CalculatorView.swift
@@ -7,9 +7,16 @@
 
 import UIKit
 
+protocol CalculatorViewDelegate: NSObject {
+    func buttonClick()
+}
+
 class CalculatorView: UIView {
     
+    var delegate: CalculatorViewController?
+    
     let resultLabel = CalculatorLabel(title: "0")
+    let subLabel = SubCalculatorLabel(title: "0")
     
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -21,7 +28,7 @@ class CalculatorView: UIView {
         super.init(coder: coder)
         setup()
     }
-
+    
     private func setup() {
         drawLabel()
         drawButton()
@@ -29,48 +36,105 @@ class CalculatorView: UIView {
     
     private func drawLabel() {
         
+        // resultLabel view에 표시
         self.addSubview(resultLabel)
         
+        // resultLabel constraints 설정
         resultLabel.snp.makeConstraints {
             $0.leading.equalTo(self.snp.leading).inset(30)
             $0.trailing.equalTo(self.snp.trailing).inset(30)
             $0.top.equalTo(self.snp.top).inset(200)
         }
+        
+        self.addSubview(subLabel)
+        
+        subLabel.snp.makeConstraints {
+            $0.leading.equalTo(self.snp.leading).inset(30)
+            $0.trailing.equalTo(self.snp.trailing).inset(30)
+            $0.bottom.equalTo(resultLabel.snp.top)
+        }
     }
     
     private func drawButton() {
-        let num0Button = CalculatorButton(title: "0")
-        let num1Button = CalculatorButton(title: "1")
-        let num2Button = CalculatorButton(title: "2")
-        let num3Button = CalculatorButton(title: "3")
-        let num4Button = CalculatorButton(title: "4")
-        let num5Button = CalculatorButton(title: "5")
-        let num6Button = CalculatorButton(title: "6")
-        let num7Button = CalculatorButton(title: "7")
-        let num8Button = CalculatorButton(title: "8")
-        let num9Button = CalculatorButton(title: "9")
         
-        let addButton = OrangeCalculatorButton(title: "+")
-        let subButton = OrangeCalculatorButton(title: "-")
-        let mulButton = OrangeCalculatorButton(title: "*")
-        let divButton = OrangeCalculatorButton(title: "/")
+        // 숫자버튼 리스트
+        let numButtonList = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
         
-        let acButton = CalculatorButton(title: "AC")
-        let resultButton = CalculatorButton(title: "=")
-                
-        let firstHorizontalStackView = CalculatorHorizontalStackView(axis: .horizontal, addArrangedSubview: [num7Button, num8Button, num9Button, addButton])
-        let secondHorizontalStackView = CalculatorHorizontalStackView(axis: .horizontal, addArrangedSubview: [num4Button, num5Button, num6Button, subButton])
-        let thirdHorizontalStackView = CalculatorHorizontalStackView(axis: .horizontal, addArrangedSubview: [num1Button, num2Button, num3Button, mulButton])
-        let fourthHorizontalStackView = CalculatorHorizontalStackView(axis: .horizontal, addArrangedSubview: [acButton, num0Button, resultButton, divButton])
+        // 숫자버튼 생성(배경색: 검정)
+        let numButtons: [CalculatorButton] = numButtonList.map {
+            CalculatorButton(title: $0)
+        }
+        
+        // 숫자버튼 클릭시 buttonClicked 호출
+        numButtons.forEach {
+            $0.addTarget(self, action: #selector(numButtonClicked), for: .touchUpInside)
+        }
+        
+        // 기능버튼 리스트
+        let funcButtonList = ["+", "-", "*", "/", "=", "ac"]
+        
+        // 기능버튼 생성(배경: 오렌지)
+        let funcButtons: [OrangeCalculatorButton] = funcButtonList.map {
+            OrangeCalculatorButton(title: $0)
+        }
+        
+        // 기능버튼 클릭시 buttonClicked 호출
+        funcButtons.forEach {
+            $0.addTarget(self, action: #selector(funcButtonClicked), for: .touchUpInside)
+        }
+        
 
-        let verticalStackview = CalculatorStackView(axis: .vertical, addArrangedSubview: [firstHorizontalStackView, secondHorizontalStackView, thirdHorizontalStackView, fourthHorizontalStackView])
+        // 버튼으로 horizontalStackView 생성
+        let firstHorizontalStackView = CalculatorHorizontalStackView(
+            axis: .horizontal,
+            addArrangedSubview: [
+                numButtons[7], numButtons[8], numButtons[9], funcButtons[0]
+            ])
+        let secondHorizontalStackView = CalculatorHorizontalStackView(
+            axis: .horizontal,
+            addArrangedSubview: [
+                numButtons[4], numButtons[5], numButtons[6], funcButtons[1]
+            ])
+        let thirdHorizontalStackView = CalculatorHorizontalStackView(
+            axis: .horizontal,
+            addArrangedSubview: [
+                numButtons[1], numButtons[2], numButtons[3], funcButtons[2]
+            ])
+        let fourthHorizontalStackView = CalculatorHorizontalStackView(
+            axis: .horizontal,
+            addArrangedSubview: [
+                funcButtons[5], numButtons[0], funcButtons[4], funcButtons[3]
+            ])
         
+
+        // horizontalStackView로 verticalStackView 생성
+        let verticalStackview = CalculatorStackView(
+            axis: .vertical,
+            addArrangedSubview: [
+                firstHorizontalStackView,
+                secondHorizontalStackView,
+                thirdHorizontalStackView,
+                fourthHorizontalStackView])
+        
+        // verticalStackview view에 표시
         self.addSubview(verticalStackview)
         
+        // verticalStackview constainrs 설정
         verticalStackview.snp.makeConstraints {
             $0.width.equalTo(350)
             $0.centerX.equalToSuperview()
             $0.top.equalTo(resultLabel.snp.bottom).offset(60)
         }
     }
+    
+    @objc
+    private func numButtonClicked() {
+        delegate?.buttonClick()
+    }
+    
+    @objc
+    private func funcButtonClicked() {
+        delegate?.buttonClick()
+    }
+    
 }
