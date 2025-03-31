630c47bdee1da26b111567c95e948d445d693c98
Chanho Lee <chanho1228@gmail.com>
2025-03-31 19:53:28 +0900

    refactor: 계산기 코드 구조 개선

--- nbcampCalculatorWithUI/Model/Calculator.swift
+++ nbcampCalculatorWithUI/Model/Calculator.swift
@@ -5,23 +5,17 @@
 //  Created by Chanho Lee on 3/31/25.
 //
 //Calculator 클래스 정의
+import OSLog
+
 public class Calculator {
-    
     
     public init() {
         
     }
     
     public func calculate(input: String) -> String {
-        
-        let add = AddOperation()
-        let sub = SubtractOperation()
-        let mul = MultiplyOperation()
-        let div = DivideOperation()
-        
-        let pattern = /\d+|[-+*\/]/
-        var array = input.matches(of: pattern).map { String($0.0) }
-        
+                
+        var array = readFromString(input: input)
         
         while true {
             
@@ -36,114 +30,96 @@ public class Calculator {
             
             switch logic {
             case (false, false):
-                print("없음")
+                let osLogMessage = "(debug) logic = \(logic)\n(fals, fals) 조건 예외처리 필요"
+                os_log(.debug, log: .default, "\(osLogMessage)")
             case (false, true), (true, true):
                 guard divIndex != nil else {
-                    print("곱셈 있음")
-                    let firstNumber = Int(array[mulIndex! - 1])!
-                    let secondNumber = Int(array[mulIndex! + 1])!
-                    let tempValue: Int = Int(try! mul.calculate(firstNumber, secondNumber))
-                    
-                    array.remove(at: mulIndex! + 1)
-                    array.remove(at: mulIndex!)
-                    array.remove(at: mulIndex! - 1)
-                    
-                    array.insert(String(tempValue), at: mulIndex! - 1)
+                    let osLogMessage = "(debug) 입력에 곱셈 포함. 나눗셈 미포함. 곱셈 먼저"
+                    os_log(.debug, log: .default, "\(osLogMessage)")
+                    array = getresult(test: MultiplyOperation(), input: array)
                     continue
                 }
                 
                 guard mulIndex != nil else {
-                    print("나눗셈 있음")
-                    let firstNumber = Int(array[divIndex! - 1])!
-                    let secondNumber = Int(array[divIndex! + 1])!
-                    let tempValue: Int = Int(try! div.calculate(firstNumber, secondNumber))
-                    
-                    array.remove(at: divIndex! + 1)
-                    array.remove(at: divIndex!)
-                    array.remove(at: divIndex! - 1)
-                    
-                    array.insert(String(tempValue), at: divIndex! - 1)
+                    let osLogMessage = "(debug) 입력에 나눗셈 포함. 곱셈 미포함. 나눗셈 먼저"
+                    os_log(.debug, log: .default, "\(osLogMessage)")
+                    array = getresult(test: DivideOperation(), input: array)
                     continue
                 }
                 
                 if Int(mulIndex!) < Int(divIndex!) {
-                    print("곱셈 먼저")
-                    let firstNumber = Int(array[mulIndex! - 1])!
-                    let secondNumber = Int(array[mulIndex! + 1])!
-                    let tempValue: Int = Int(try! mul.calculate(firstNumber, secondNumber))
-                    
-                    array.remove(at: mulIndex! + 1)
-                    array.remove(at: mulIndex!)
-                    array.remove(at: mulIndex! - 1)
-                    
-                    array.insert(String(tempValue), at: mulIndex! - 1)
+                    let osLogMessage = "(debug) 입력에 곱셈, 나눗셈 포함. 곱셈이 먼저 있음"
+                    os_log(.debug, log: .default, "\(osLogMessage)")
+                    array = getresult(test: MultiplyOperation(), input: array)
                 } else {
-                    print("나눗셈 먼저")
-                    let firstNumber = Int(array[divIndex! - 1])!
-                    let secondNumber = Int(array[divIndex! + 1])!
-                    let tempValue: Int = Int(try! div.calculate(firstNumber, secondNumber))
-                    
-                    array.remove(at: divIndex! + 1)
-                    array.remove(at: divIndex!)
-                    array.remove(at: divIndex! - 1)
-                    
-                    array.insert(String(tempValue), at: divIndex! - 1)
+                    let osLogMessage = "(debug) 입력에 곱셈, 나눗셈 포함. 나눗셈이 먼저 있음"
+                    os_log(.debug, log: .default, "\(osLogMessage)")
+                    array = getresult(test: DivideOperation(), input: array)
                 }
                 
             case (true, false): //덧셈뺄셈만 있음
                 guard subIndex != nil else {
-                    print("덧셈만 있음")
-                    let firstNumber = Int(array[addIndex! - 1])!
-                    let secondNumber = Int(array[addIndex! + 1])!
-                    let tempValue: Int = Int(try! add.calculate(firstNumber, secondNumber))
-                    
-                    array.remove(at: addIndex! + 1)
-                    array.remove(at: addIndex!)
-                    array.remove(at: addIndex! - 1)
-                    
-                    array.insert(String(tempValue), at: addIndex! - 1)
+                    let osLogMessage = "(debug) 입력에 덧셈만 있음"
+                    os_log(.debug, log: .default, "\(osLogMessage)")
+                    array = getresult(test: AddOperation(), input: array)
                     continue
                 }
                 
                 guard addIndex != nil else {
-                    print("뺄셈만 있음")
-                    let firstNumber = Int(array[subIndex! - 1])!
-                    let secondNumber = Int(array[subIndex! + 1])!
-                    let tempValue: Int = Int(try! sub.calculate(firstNumber, secondNumber))
-                    
-                    array.remove(at: subIndex! + 1)
-                    array.remove(at: subIndex!)
-                    array.remove(at: subIndex! - 1)
-                    
-                    array.insert(String(tempValue), at: subIndex! - 1)
+                    let osLogMessage = "(debug) 입력에 뺄셈만 있음"
+                    os_log(.debug, log: .default, "\(osLogMessage)")
+                    array = getresult(test: SubtractOperation(), input: array)
                     continue
                 }
                 
                 if Int(addIndex!) < Int(subIndex!) {
-                    print("덧셈 먼저")
-                    let firstNumber = Int(array[addIndex! - 1])!
-                    let secondNumber = Int(array[addIndex! + 1])!
-                    let tempValue: Int = Int(try! add.calculate(firstNumber, secondNumber))
-                    
-                    array.remove(at: addIndex! + 1)
-                    array.remove(at: addIndex!)
-                    array.remove(at: addIndex! - 1)
-                    
-                    array.insert(String(tempValue), at: addIndex! - 1)
+                    let osLogMessage = "(debug) 입력에 덧셈, 뺄셈 포함. 덧셈이 먼저 있음"
+                    os_log(.debug, log: .default, "\(osLogMessage)")
+                    array = getresult(test: AddOperation(), input: array)
                 } else {
-                    print("뺄셈 먼저")
-                    let firstNumber = Int(array[subIndex! - 1])!
-                    let secondNumber = Int(array[subIndex! + 1])!
-                    let tempValue: Int = Int(try! sub.calculate(firstNumber, secondNumber))
-                    
-                    array.remove(at: subIndex! + 1)
-                    array.remove(at: subIndex!)
-                    array.remove(at: subIndex! - 1)
-                    
-                    array.insert(String(tempValue), at: subIndex! - 1)
+                    let osLogMessage = "(debug) 입력에 덧셈, 뺄셈 포함. 뺄셈이 먼저 있음"
+                    os_log(.debug, log: .default, "\(osLogMessage)")
+                    array = getresult(test: SubtractOperation(), input: array)
                 }
             }
         }
         return array[0]
     }
+    
+    func readFromString(input: String) -> [String] {
+        let pattern = /\d+|[-+*\/]/
+        return input.matches(of: pattern).map { String($0.0) }
+    }
+    
+    func getresult(test: Operation, input: [String]) -> [String] {
+        
+        var inputArray = input
+        var index = 0
+        
+        switch test {
+        case is AddOperation:
+            index = inputArray.firstIndex(of: "+")!
+        case is SubtractOperation:
+            index = inputArray.firstIndex(of: "-")!
+        case is MultiplyOperation:
+            index = inputArray.firstIndex(of: "*")!
+        case is DivideOperation:
+            index = inputArray.firstIndex(of: "/")!
+        default:
+            print("4")
+        }
+        
+        let firstNumber = Double(inputArray[index - 1])!
+        let secondNumber = Double(inputArray[index + 1])!
+        let tempValue: Int = Int(try! test.calculate(firstNumber, secondNumber))
+        
+        inputArray.remove(at: index + 1)
+        inputArray.remove(at: index)
+        inputArray.remove(at: index - 1)
+        
+        inputArray.insert(String(tempValue), at: index - 1)
+        
+        return inputArray
+    }
+
 }
