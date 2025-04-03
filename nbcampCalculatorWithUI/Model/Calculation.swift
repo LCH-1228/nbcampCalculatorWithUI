ee752e5da038b7de5179ae9d831637e513fb0816
Chanho Lee <chanho1228@gmail.com>
2025-04-03 11:57:56 +0900

    refactor: 연산 클래스에서 불필요한 타입 변환 제거 및 주석 추가

--- nbcampCalculatorWithUI/Model/Calculation.swift
+++ nbcampCalculatorWithUI/Model/Calculation.swift
@@ -6,48 +6,48 @@
 //
 import Foundation
 
+//CalculationProtocol 정의
 protocol CalculationProtocol {
     
+    //CalculationProtocol에서는 String 타입의 매개변수 firstNumber와 secondNumber를 가지고 String을 반환하는 calculate 함수 구현이 필요함을 정의
     func calculate(_ firstNumber: String, _ secondNumber: String) throws -> String
     
 }
 
+// CalculationProtocol을 채택하는 덧셈 기능의 AddCalculation 클래스
 class AddCalculation: CalculationProtocol {
     
     func calculate(_ firstNumber: String, _ secondNumber: String) throws -> String {
-        let decimalNum1 = NSDecimalNumber(string: firstNumber)
-        let decimalNum2 = NSDecimalNumber(string: secondNumber)
+        let decimalNum1 = NSDecimalNumber(string: firstNumber) //String을 Decimal로 연산하기 위해 NSDecimalNumber로 변환
+        let decimalNum2 = NSDecimalNumber(string: secondNumber) //String을 Decimal로 연산하기 위해 NSDecimalNumber로 변환
         
-        let result = (decimalNum1 as Decimal) + (decimalNum2 as Decimal)
-        
-        return NSDecimalNumber(decimal: result).stringValue
+        return decimalNum1.adding(decimalNum2).stringValue //덧셈 결과 NSDecimalNumber를 String으로 반환
     }
 }
 
+// CalculationProtocol을 채택하는 뺄셈 기능의 SubtractCalculation 클래스
 class SubtractCalculation: CalculationProtocol {
     
     func calculate(_ firstNumber: String, _ secondNumber: String) throws -> String {
-        let decimalNum1 = NSDecimalNumber(string: firstNumber)
-        let decimalNum2 = NSDecimalNumber(string: secondNumber)
-        
-        let result = (decimalNum1 as Decimal) - (decimalNum2 as Decimal)
-        
-        return NSDecimalNumber(decimal: result).stringValue
+        let decimalNum1 = NSDecimalNumber(string: firstNumber) //String을 Decimal로 연산하기 위해 NSDecimalNumber로 변환
+        let decimalNum2 = NSDecimalNumber(string: secondNumber) //String을 Decimal로 연산하기 위해 NSDecimalNumber로 변환
+                
+        return decimalNum1.subtracting(decimalNum2).stringValue //뺄셈 결과 NSDecimalNumber를 String으로 반환
     }
 }
 
+// CalculationProtocol을 채택하는 곱셈 기능의 MultiplyCalculation 클래스
 class MultiplyCalculation: CalculationProtocol {
     
     func calculate(_ firstNumber: String, _ secondNumber: String) throws -> String {
-        let decimalNum1 = NSDecimalNumber(string: firstNumber)
-        let decimalNum2 = NSDecimalNumber(string: secondNumber)
-        
-        let result = (decimalNum1 as Decimal) * (decimalNum2 as Decimal)
-        
-        return NSDecimalNumber(decimal: result).stringValue
+        let decimalNum1 = NSDecimalNumber(string: firstNumber) //String을 Decimal로 연산하기 위해 NSDecimalNumber로 변환
+        let decimalNum2 = NSDecimalNumber(string: secondNumber) //String을 Decimal로 연산하기 위해 NSDecimalNumber로 변환
+                
+        return decimalNum1.multiplying(by: decimalNum2).stringValue //곱셈 결과 NSDecimalNumber를 String으로 반환
     }
 }
 
+// CalculationProtocol을 채택하는 나눗셈 기능의 DivideCalculation 클래스
 class DivideCalculation: CalculationProtocol {
     
     func calculate(_ firstNumber: String, _ secondNumber: String) throws -> String {
@@ -57,11 +57,9 @@ class DivideCalculation: CalculationProtocol {
         guard secondNumber != "0" else {
             throw CustomError.devidedByZero
         }
-        let decimalNum1 = NSDecimalNumber(string: firstNumber)
-        let decimalNum2 = NSDecimalNumber(string: secondNumber)
+        let decimalNum1 = NSDecimalNumber(string: firstNumber) //String을 Decimal로 연산하기 위해 NSDecimalNumber로 변환
+        let decimalNum2 = NSDecimalNumber(string: secondNumber) //String을 Decimal로 연산하기 위해 NSDecimalNumber로 변환
         
-        let result = (decimalNum1 as Decimal) / (decimalNum2 as Decimal)
-
-        return NSDecimalNumber(decimal: result).stringValue
+        return decimalNum1.dividing(by: decimalNum2).stringValue //나눗셈 결과 NSDecimalNumber를 String으로 반환
     }
 }
